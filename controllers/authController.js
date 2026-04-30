85a405b29aecd67b04210516c8f3833db2bd12b5
ephraim64 <tettehephraim.64@gmail.com>
2026-04-30 06:39:20 +0000

    feat: fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -368,7 +368,7 @@ export const login = async (req, res) => {
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
-    const isMatch = await bcrypt.compare(passwor, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res
         .status(400)
